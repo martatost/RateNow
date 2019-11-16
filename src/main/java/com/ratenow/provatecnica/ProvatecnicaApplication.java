0227617cb21bcbf64773b52336fd7a37b8ac40a7
Marta Tost LLopis <marta.tost@gmail.com>
2019-11-16 13:16:24 +0100

    Projecte Spring Boot bàsic

---
+++ src/main/java/com/ratenow/provatecnica/ProvatecnicaApplication.java
@@ -0,0 +1,13 @@
+package com.ratenow.provatecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvatecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvatecnicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ratenow/provatecnica/ProvatecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratenow.provatecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvatecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
